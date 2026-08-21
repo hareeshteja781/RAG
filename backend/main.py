b901c03780e94cc4409da19fc0bc68659e8ab017
Hareesh Teja Paruchuri <hareeshtejaparuchuri@gmail.com>
2026-08-21 14:53:57 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-teal.vercel.app"],
+    allow_origins=["https://enterprise-rag-frontend-teal.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
